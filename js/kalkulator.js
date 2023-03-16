49d4bf1d5c4b20ba16836a5894e21534efbf7033
alfiyahnura <alfiyahnur107@gmail.com>
2023-03-16 07:01:36 +0700

    latihan 15 js

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -2,14 +2,49 @@ function tambah(){
     var frm = document.getElementById('kalkulator');
     var a1 = parseFloat(frm.angka.value);
     var a2 = parseFloat(frm.angka2.value);
-    var total = a1 + a2;
-    frm.hasil.value = total;
+    if(total = a1 + a2){
+        frm.hasil.value = total;
+    }else{
+        frm.hasil.value = 'Maaf angka belum terisi';
+    }
 }
-
 function kurang(){
     var frm = document.getElementById('kalkulator');
     var a1 = parseFloat(frm.angka.value);
     var a2 = parseFloat(frm.angka2.value);
-    var total = a1 - a2;
-    frm.hasil.value = total;
+    if(total = a1 - a2){
+        frm.hasil.value = total;
+    }else{
+        frm.hasil.value = 'Maaf angka belum terisi';
+    }
+}
+function kali(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    if(total = a1 * a2){
+        frm.hasil.value = total;
+    }else{
+        frm.hasil.value = 'Maaf angka belum terisi';
+    }
+}
+function bagi(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    if(total = a1 / a2){
+        frm.hasil.value = total;
+    }else{
+        frm.hasil.value = 'Maaf angka belum terisi';
+    }
+}
+function pangkat(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    if(total = a1 ** a2){
+        frm.hasil.value = total;
+    }else{
+        frm.hasil.value = 'Maaf angka belum terisi';
+    }
 }
